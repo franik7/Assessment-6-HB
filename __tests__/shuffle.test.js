48dedee17852018b42abbeb4aaeaa95442392e92
Frants <frants.kavalionak@live.com>
2024-02-24 19:39:29 -0500

    All Parts

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,12 +1,17 @@
 const shuffle = require("../src/shuffle");
 
+
+
 describe("shuffle should...", () => {
   // CODE HERE
   test("return an array", () => {
-    expect(Array.isArray(['result'])).toBe(true);
+    let array = shuffle(['robot1', 'robot2', 'robot3'])
+    expect(Array.isArray(array)).toBe(true);
       })
 
   test("check that it returns an array of the same length as the argument sent in", () => {
-    expect(shuffle(array.length)).toHaveLength(shuffle(result.lengt));
+    let original = ['robot1', 'robot2', 'roboot3'];
+    let shuffled = shuffle(original);
+    expect(original.length).toEqual(shuffled.length);
       })
 });
